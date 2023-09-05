b57def96ed41ffb7aac7d22ca79a0becb8afa198
alikemaluysal <alikemaluysal4235@gmail.com>
2023-09-05 20:36:12 +0300

    ORM vs SQL

---
+++ 1_ORM/ORM.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+
+namespace _1_ORM
+{
+    public class NorthwindDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            ConfigurationManager configuration = new();
+            configuration.AddJsonFile("appsettings.json");
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("NorthwindConStr"));
+        }
+    }
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string? Title { get; set; }
+        public string? TitleOfCourtesy { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public DateTime? HireDate { get; set; }
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string? PostalCode { get; set; }
+        public string? Country { get; set; }
+        public string? HomePhone { get; set; }
+        public string? Extension { get; set; }
+        public string? Notes { get; set; }
+        public int? ReportsTo { get; set; }
+        public string? PhotoPath { get; set; }
+        public string? Region { get; set; }
+    }
+}
---
+++ 1_ORM/Program.cs
@@ -0,0 +1,40 @@
+﻿using _1_ORM;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+#region SQL - Bad Practice
+
+Console.WriteLine("Without ORM: ");
+
+ConfigurationManager configuration = new();
+configuration.AddJsonFile("appsettings.json");
+
+await using SqlConnection connection = new(configuration.GetConnectionString("NorthwindConStr"));
+await connection.OpenAsync();
+
+SqlCommand command = new("Select * from Employees", connection);
+SqlDataReader dr = await command.ExecuteReaderAsync();
+while (await dr.ReadAsync())
+{
+    Console.WriteLine($"{dr["FirstName"]} {dr["LastName"]}");
+}
+await connection.CloseAsync();
+#endregion
+
+Console.WriteLine();
+
+#region ORM - Best Practice
+
+Console.WriteLine("With ORM: ");
+
+
+NorthwindDbContext context = new();
+var employees = await context.Employees.ToListAsync();
+
+foreach (var employee in employees)
+{
+    Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+}
+
+#endregion
